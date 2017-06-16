45ac7f3484008799868cd1a931fba8540f03a9b6
David Fialho <fialho.david@protonmail.com>
2017-06-16 09:49:16 +0100

    Initial structure of the project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Episode Tracker
+Tool to track new episodes from TV Shows
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tveebot-tracker',
+    version='0.1',
+    description='Tool to track new episodes from TV Shows',
+    url='https://github.com/tveebot/tracker',
+    license='MIT',
+    author='David Fialho',
+    author_email='fialho.david@protonmail.com',
+
+    packages=find_packages(),
+)
