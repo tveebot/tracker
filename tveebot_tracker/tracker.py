622aec8ff41443707d8c3cef5022426d355e9abf
David Fialho <fialho.david@protonmail.com>
2017-11-04 12:24:41 +0000

    Finish defining Tracker interface and dependencies

--- tveebot_tracker/tracker.py
+++ tveebot_tracker/tracker.py
@@ -1,9 +1,11 @@
-from tveebot_tracker.downloader import Downloader
+from queue import Queue
+
 from tveebot_tracker.episode_db import EpisodeDB
 from tveebot_tracker.source import EpisodeSource
+from tveebot_tracker.stoppable_thread import StoppableThread
 
 
-class Tracker:
+class Tracker(StoppableThread):
     """
     The Tracker is the main component of the application. It connects
     everything together.
@@ -14,16 +16,22 @@ class Tracker:
     """
 
     def __init__(self, source: EpisodeSource, episode_db: EpisodeDB,
-                 downloader: Downloader):
+                 download_queue: Queue, check_period: float):
+        """
+        Initialize the tracker with the necessary components.
+
+        :param source:         source to obtain episode files from
+        :param episode_db:     DB used to track episodes
+        :param download_queue: queue shared with downloader to place new
+                               episodes to be downloaded
+        :param check_period:   period of time with which the tracker will
+                               check for new episodes
+        """
+        super().__init__()
         self.source = source
-        self.episode_db = episode_db
-        self.downloader = downloader
+        self.database = episode_db
+        self._queue = download_queue
+        self._check_period = check_period
 
-    def run_forever(self):
-        pass
-
-    def stop(self):
-        pass
-
-    def close(self):
+    def run(self):
         pass
